039fff5ff1a30b46ea690ff498b9d97ccc400270
Pooja Gurung <147177106+Poozagg@users.noreply.github.com>
2025-07-24 15:47:21 +0100

    Fix Puma bind for deployment
    REnder deployment

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,7 @@
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
 
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -22,8 +23,10 @@ end
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Bind the server to all IP addresses (0.0.0.0) and use the port defined by the environment variable.
+# Required for deployment platforms like Render, which expect the app to listen on 0.0.0.0:$PORT
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
+
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
@@ -33,3 +36,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
